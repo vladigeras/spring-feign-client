81291bf9330c3458c76f38c47c69d604dc74d82b
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2020-01-11 23:21:54 +0300

    Initial commit

---
+++ src/main/java/ru/vladigeras/springfeignclient/SpringFeignClientApplication.java
@@ -0,0 +1,13 @@
+package ru.vladigeras.springfeignclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFeignClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vladigeras/springfeignclient/SpringFeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladigeras.springfeignclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
